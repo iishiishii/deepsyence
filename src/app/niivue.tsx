df82043573553a679291c24d03382b38d94dac6d
iishiishii <love_sweet_98@yahoo.com.vn>
2023-08-09 23:36:32 +1000

    refactor

--- src/app/niivue.tsx
+++ src/app/niivue.tsx
@@ -16,15 +16,11 @@ import { onnxMaskToImage } from "./helpers/maskUtils";
 import { modelData } from "./helpers/onnxModelAPI";
 import AppContext from "./hooks/createContext";
 import { InferenceSession, Tensor } from "onnxruntime-web";
+import { viewType } from "./types";
 /* @ts-ignore */
 import npyjs from "npyjs";
 import * as ort from "onnxruntime-web";
 import { nv3dModelPostProcess, nvPostSam, updateSliceType } from "./helpers/niivueHandler";
-
-// Define image, embedding and model paths
-// const IMAGE_PATH = "/assets/data/dogs.jpg";
-const IMAGE_EMBEDDING = new URL("./model/head.npy", document.baseURI).href;
-const MODEL_DIR = new URL("./model/sam_onnx_quantized_example.onnx", document.baseURI).href;
 
 const theme = createTheme({
   palette: {
@@ -47,63 +43,16 @@ const nv = new Niivue({
   onLocationChange: handleIntensityChange,
 });
 
-function handleIntensityChange(data) {
-  document.getElementById("intensity").innerHTML = "&nbsp;&nbsp;" + data.string;
+function handleIntensityChange(data: any) {
+  document.getElementById("intensity")!.innerHTML = "&nbsp;&nbsp;" + data.string;
 }
 
 // let instance = new WorkerBuilder(Worker);
 
-// The NiiVue component wraps all other components in the UI.
-// It is exported so that it can be used in other projects easily
-export default function NiiVue(props) {
+export default function NiiVue(props: any) {
   const [openLayers, setOpenLayers] = useState(false);
   const [layers, setLayers] = useState(nv.volumes);
-  const [selectedLayer, setSelectedLayer] = [] || useState(nv.volumes[0]);
-  const {
-    clicks: [clicks],
-    image: [, setImage],
-    maskImg: [, setMaskImg],
-  } = useContext(AppContext);
-  const [model, setModel] = useState(null); // ONNX model
-  const [tensor, setTensor] = useState(null); // Image embedding tensor
-
-  // The ONNX model expects the input to be rescaled to 1024. 
-  // The modelScale state variable keeps track of the scale values.
-  const [modelScale, setModelScale] = useState(null);
-
-  // Initialize the ONNX model. load the image, and load the SAM
-  // pre-computed image embedding
-  useEffect(() => {
-    // Initialize the ONNX model
-    const initModel = async () => {
-      try {
-        if (MODEL_DIR === undefined) return;
-        ort.env.wasm.wasmPaths = new URL("./js/", document.baseURI).href;
-        const MODEL_URL = MODEL_DIR;
-        const model = await InferenceSession.create(MODEL_URL, {
-          executionProviders: ["wasm"],
-        });
-        setModel(model);
-      } catch (e) {
-        console.log(e);
-      }
-    };
-    initModel();
-
-    // Load the Segment Anything pre-computed embedding
-    Promise.resolve(loadNpyTensor(IMAGE_EMBEDDING, "float32")).then(
-      (embedding) => setTensor(embedding)
-    );
-  }, []);
-
-  // Decode a Numpy file into a tensor. 
-  const loadNpyTensor = async (tensorFile, dType) => {
-    console.log(tensorFile)
-    let npLoader = new npyjs();
-    const npArray = await npLoader.load(tensorFile);
-    const tensor = new ort.Tensor(dType, npArray.data, npArray.shape);
-    return tensor;
-  };
+  const [selectedLayer, setSelectedLayer] = useState([]);
 
   nv.onImageLoaded = () => {
     setLayers([...nv.volumes]);
@@ -111,7 +60,7 @@ export default function NiiVue(props) {
 
   console.log(`layer name ${nv.volumes.length}`);
 
-  const layerList = layers.map((layer) => {
+  const layerList = layers.map((layer: any) => {
     console.log(`layer list ${layer.name}`);
     return (
       <Layer
@@ -127,11 +76,13 @@ export default function NiiVue(props) {
     );
   });
 
-  function nvSelect(layer) {
+  function nvSelect(layer: any) {
+    if (layer) {
     setSelectedLayer(layer);
+    }
   }
 
-  async function addLayer(file) {
+  async function addLayer(file: File) {
     const nvimage = await NVImage.loadFromFile({
       file: file,
     });
@@ -145,30 +96,30 @@ export default function NiiVue(props) {
     setOpenLayers(!openLayers);
   }
 
-  function nvUpdateSliceType(newSliceType) {
+  function nvUpdateSliceType(newSliceType: viewType) {
     updateSliceType(nv, newSliceType);
   }
 
-  function nvUpdateColorMap(id, clr) {
+  function nvUpdateColorMap(id: string, clr: string) {
     nv.setColormap(id, clr);
     nv.updateGLVolume();
   }
 
-  function nvRemoveLayer(imageToRemove) {
+  function nvRemoveLayer(imageToRemove: any) {
     nv.removeVolume(imageToRemove);
     setLayers([...nv.volumes]);
   }
 
-  function nvUpdateOpacity(id, opacity) {
+  function nvUpdateOpacity(id: string, opacity: number) {
     nv.setOpacity(nv.getVolumeIndexByID(id), opacity);
     nv.updateGLVolume();
   }
 
-  function nvModel(id, name, array) {
+  function nvModel(id: any, name: any, array: Float32Array) {
     nvPostSam(nv, id, name, array);
   }
 
-  function nvPreprocess(id, name, array) {
+  function nvPreprocess(id: any, name: any, array: Float32Array) {
     nv3dModelPostProcess(nv, id, name, array, setLayers)
   }
 
