e9d15b3559c67188871bd89799e2115a591ddec0
iishiishii <love_sweet_98@yahoo.com.vn>
2024-04-26 10:41:30 +1000

    Add workbox-webpack-plugin to package.json and configure maximumFileSizeToCacheInBytes in config-overrides.js

--- config-overrides.js
+++ config-overrides.js
@@ -1,4 +1,5 @@
 const webpack = require("webpack");
+const WorkBoxPlugin = require("workbox-webpack-plugin");
 
 module.exports = function override(config) {
   const fallback = config.resolve.fallback || {};
@@ -16,6 +17,12 @@ module.exports = function override(config) {
     path: require.resolve("path-browserify"),
   });
   config.resolve.fallback = fallback;
+  config.plugins.forEach((plugin) => {
+    if (plugin instanceof WorkBoxPlugin.InjectManifest) {
+      plugin.config.maximumFileSizeToCacheInBytes = 50 * 1024 * 1024;
+    }
+  });
+
   config.plugins = (config.plugins || []).concat([
     new webpack.ProvidePlugin({
       process: "process/browser",
