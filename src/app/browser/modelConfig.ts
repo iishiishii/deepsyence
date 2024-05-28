fbce70f2610d9bed17a53d23bac38d84562e69cf
iishiishii <love_sweet_98@yahoo.com.vn>
2024-05-28 22:14:44 +1000

    fix model config

--- src/app/browser/modelConfig.ts
+++ src/app/browser/modelConfig.ts
@@ -30,36 +30,34 @@ export const ListImageModels = (
   });
 };
 
+const classificationExamples: string[] = [
+  "https://web-ai-models.org/images/classification/image-1.jpg",
+  "https://web-ai-models.org/images/classification/image-2.jpg",
+  "https://web-ai-models.org/images/classification/image-3.jpg",
+  "https://web-ai-models.org/images/classification/image-4.jpg",
+  "https://web-ai-models.org/images/classification/image-5.jpg",
+  "https://web-ai-models.org/images/classification/image-6.jpg",
+];
+
+const segmentationExamples: string[] = [
+  "https://web-ai-models.org/images/segmentation/image-1.jpg",
+  "https://web-ai-models.org/images/segmentation/image-2.jpg",
+  "https://web-ai-models.org/images/segmentation/image-3.jpg",
+  "https://web-ai-models.org/images/segmentation/image-4.jpg",
+  "https://web-ai-models.org/images/segmentation/image-5.jpg",
+  "https://web-ai-models.org/images/segmentation/image-6.jpg",
+];
+
+const superresExamples: string[] = [
+  "https://web-ai-models.org/images/superres/butterfly.png",
+  "https://web-ai-models.org/images/superres/shanghai.jpg",
+  "https://web-ai-models.org/images/superres/tree.jpg",
+];
+
 export const models: ImageMetadata[] = [
   {
-    id: "lite-medsam",
-    title: "LiteMedSAM",
-    description: "",
-    memEstimateMB: 2600,
-    type: ModelType.SegmentAnything,
-    sizeMB: 377,
-    configPath:
-      "https://web-ai-models.org/image/feature-extraction/EfficientFormer/config.json",
-    modelPaths: new Map<string, string>([
-      [
-        "encoder",
-        new URL("./model/lite_medsam_encoder.onnx", document.baseURI).href,
-      ],
-      [
-        "decoder",
-        new URL("./model/lite_medsam_decoder.onnx", document.baseURI).href,
-      ],
-    ]),
-    preprocessorPath: new URL(
-      "./model/lite-medsam-preprocess.json",
-      document.baseURI
-    ).href,
-    tags: ["lite-medsam"],
-    referenceURL: "https://huggingface.co/visheratin/segment-anything-vit-b",
-  },
-  {
-    id: "efficient-sam",
-    title: "Efficient SAM quantized",
+    id: "segment-anything-quant",
+    title: "Segment Anything quantized",
     description: "",
     memEstimateMB: 2600,
     type: ModelType.SegmentAnything,
@@ -69,20 +67,40 @@ export const models: ImageMetadata[] = [
     modelPaths: new Map<string, string>([
       [
         "encoder",
-        new URL("./model/efficient_sam_vitt_encoder.onnx", document.baseURI)
-          .href,
+        "https://object-store.rc.nectar.org.au/v1/AUTH_bdf528c1856c401b9a6fcfc700260330/deepsyence/efficient_sam_vitt_encoder.onnx",
       ],
       [
         "decoder",
-        new URL("./model//efficient_sam_vitt_decoder.onnx", document.baseURI)
-          .href,
+        "https://object-store.rc.nectar.org.au/v1/AUTH_bdf528c1856c401b9a6fcfc700260330/deepsyence/efficient_sam_vitt_decoder.onnx",
       ],
     ]),
-    preprocessorPath: new URL(
-      "./model/efficient-sam-preprocess.json",
-      document.baseURI
-    ).href,
-    tags: ["efficient-sam"],
+    preprocessorPath:
+      "https://huggingface.co/visheratin/segment-anything-vit-b/resolve/main/preprocessor_config.json",
+    tags: ["segment-anything"],
+    referenceURL: "https://huggingface.co/visheratin/segment-anything-vit-b",
+  },
+  {
+    id: "efficient-sam",
+    title: "Segment Anything",
+    description: "",
+    memEstimateMB: 2600,
+    type: ModelType.SegmentAnything,
+    sizeMB: 377,
+    configPath:
+      "https://web-ai-models.org/image/feature-extraction/EfficientFormer/config.json",
+    modelPaths: new Map<string, string>([
+      [
+        "encoder",
+        "https://huggingface.co/visheratin/segment-anything-vit-b/resolve/main/encoder.onnx",
+      ],
+      [
+        "decoder",
+        "https://huggingface.co/visheratin/segment-anything-vit-b/resolve/main/decoder.onnx",
+      ],
+    ]),
+    preprocessorPath:
+      "https://huggingface.co/visheratin/segment-anything-vit-b/resolve/main/preprocessor_config.json",
+    tags: ["segment-anything"],
     referenceURL: "https://huggingface.co/visheratin/segment-anything-vit-b",
   },
 ];
