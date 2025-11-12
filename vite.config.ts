e06703808c5af58c7da224a45e5fc8f4e5d390ad
Thuy Dao <love_sweet_98@yahoo.com.vn>
2025-11-12 14:43:10 +1000

    set base url in vite config

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,7 @@ import { tanstackRouter } from "@tanstack/router-plugin/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss(), tanstackRouter()],
+  base: "/deepsyence/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
